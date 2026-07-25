e215bc09b8496fab5a4d955e2895a45b0474b413
changgretchen52-maker <changgretchen52@gmail.com>
2026-07-26 02:03:15 +0800

    fix database init

--- db.js
+++ db.js
@@ -1,10 +1,16 @@
 const Database = require('better-sqlite3');
 const path = require('path');
+const fs = require('fs');
 
-const db = new Database(
-  path.join(__dirname, 'storysite.db')
-);
+const dbPath = path.join(__dirname, 'storysite.db');
+
+const db = new Database(dbPath);
+
+const initPath = path.join(__dirname, 'init.sql');
+
+if (fs.existsSync(initPath)) {
+  const sql = fs.readFileSync(initPath, 'utf8');
+  db.exec(sql);
+}
 
 module.exports = db;
-
-
