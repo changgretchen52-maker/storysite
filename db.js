276dc70678c7ac9c1e380824167de3a36cef5a7e
changgretchen52-maker <changgretchen52@gmail.com>
2026-07-26 02:11:56 +0800

    fix sqlite connection

--- db.js
+++ db.js
@@ -1,16 +1,8 @@
 const Database = require('better-sqlite3');
 const path = require('path');
-const fs = require('fs');
 
-const dbPath = path.join(__dirname, 'storysite.db');
-
-const db = new Database(dbPath);
-
-const initPath = path.join(__dirname, 'init.sql');
-
-if (fs.existsSync(initPath)) {
-  const sql = fs.readFileSync(initPath, 'utf8');
-  db.exec(sql);
-}
+const db = new Database(
+  path.join(__dirname, 'storysite.db')
+);
 
 module.exports = db;
